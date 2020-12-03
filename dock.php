ad69fe8bfe6f5535e2c106cffed2fe15a79ab32f
ZN29 <2137929135@qq.com>
2020-12-03 21:06:23 +0800

    添加主文件

---
+++ dock.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+      <link rel="stylesheet" href="http://znuo.top:29/bigsur/dock/css/style.css">
+      </head>
+
+<body>
+  <div id="container">
+    <div id="dock">
+      <ul>
+				<li>
+          <span>Finder</span> <a href="#"><img src=
+          "https://icons.iconarchive.com/icons/dtafalonso/ios8/512/Finder-icon.png">
+          </a>
+        <li>
+          <span>Launchpad</span> <a href="#"><img src=
+          "http://znuo.top:29/bigsur/dock/images/launchpad-icon.png"></a>
+        </li>
+        <li>
+          <span>Chrome</span> <a href="#"><img src=
+          "https://icons.iconarchive.com/icons/dakirby309/simply-styled/256/Google-Chrome-icon.png">
+          </a>
+        </li>
+        <li>
+          <span>App Store</span> <a href="#"><img src=
+          "http://znuo.top:29/bigsur/dock/images/appstore.png"></a>
+        </li>
+        <li>
+          <span>System pereferences</span> <a href="#"><img src=
+          "http://icons.iconarchive.com/icons/johanchalibert/mac-osx-yosemite/1024/system-preferences-icon.png"></a>
+        </li>
+				<li>
+          <span>Siri</span> <a href="#"><img src=
+          "http://znuo.top:29/bigsur/dock/images/siri.png"></a>
+        </li>
+				<li>
+          <span>iTunes</span> <a href="#"><img src=
+          "http://znuo.top:29/bigsur/dock/images/iTunes 1024x1024.png"></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+
+	<script src="<?php $this->options->themeUrl('assets/js/001.js'); ?>"></script>
+	<script src="<?php $this->options->themeUrl('assets/js/002.js'); ?>"></script>
+	<div class="footer">
+		<p>Powered by Typecho | Theme by BigSur</a></p>
+		<!--尊重作者，保留版权信息，如果强烈需要去掉，希望能捐赠作者一杯咖啡，捐赠地址：https://www.vpsmm.com/pay.shtml 谢谢-->
+	</div>
+
+
+<?php $this->footer(); ?>
+</body>
+</html>
